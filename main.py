9f48ea37e79d57d8f97dca13b3182e3282761035
Alessandro Cogliati <51083245+alessandrocogliati@users.noreply.github.com>
2021-09-29 17:05:32 +0200

    pca start

--- main.py
+++ main.py
@@ -6,29 +6,28 @@ Created on Wed Sep 22 14:44:25 2021
 """
 
 import pandas as pd
+from scipy import stats
+from sklearn.preprocessing import MinMaxScaler
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.decomposition import PCA
+import os
+import matplotlib.image as mpimg
 
 filename = 'data.csv'
 df = pd.read_csv(filename, sep=';')  # gives a dataframe
 # raw_data = df.values            # convert to array
 
-
 ### first remove the most extreme outliers
-
 df = df[df['time'] < 3.0]
 df = df.reset_index(drop=True)
 
 ### standardize for each person
-from scipy import stats
-
 for elem_id in df['id'].unique():
     df.loc[df['id'] == elem_id, 'time'] = stats.zscore(df.loc[df['id'] == elem_id, 'time'], ddof=1)
 
 ### convert to a continuous index
-
-from sklearn.preprocessing import MinMaxScaler
-
 ## put Non-smiling in the range [-1,0), and the most negative value is the fastest
-
 mask_not_smiling = df.label == 'Not smiling'
 df.loc[mask_not_smiling, 'time'] = MinMaxScaler(feature_range=(-1, 0)).fit_transform(df[mask_not_smiling]['time'].values.reshape(-1, 1))
 
@@ -37,33 +36,64 @@ mask_smiling = df.label == 'Smiling'
 df.loc[mask_smiling, 'time'] = - df.loc[mask_smiling, 'time']
 df.loc[mask_smiling, 'time'] = MinMaxScaler(feature_range=(0, 1)).fit_transform(df[mask_smiling]['time'].values.reshape(-1, 1))
 
-# ### THRESHOLD, NOT DOING IT
-# m_s = (df.loc[mask_smiling, 'time']).mean()
-#
-# # mean for both labels
-# m_ns = (df.loc[mask_not_smiling, 'time']).mean()
-#
-# # set to -1 the ones below the mean
-#
-# df_not_smiling = df[mask_not_smiling]['time']
-# mask_inside_not_smiling = df_not_smiling < m_ns
-#
-# import numpy as np
-# df_not_smiling = np.where(mask_inside_not_smiling, -1, df_not_smiling.values)
-# df_not_smiling = MinMaxScaler(feature_range=(-1, 0)).fit_transform(df_not_smiling.reshape(-1, 1))
-# df.loc[mask_not_smiling, 'time'] = df_not_smiling
-#
-# # set to 1 the ones above the mean
-# if df[mask, 'time'] > m_s:
-#     df.loc[mask, 'time'] = 1
+plt.gray()
+plt.axis('off')
 
-## now all the values are rescaled in the correct ranges
+# def load_images(folder, outliers):
+#   images = []
+#   files_to_load = os.listdir(folder)
+#   for outlier in outliers:
+#     files_to_load = files_to_load.remove(outlier)
+#   for filename in files_to_load:
+#     img = mpimg.imread(os.path.join(folder, filename))
+#     if img is not None:
+#       img = list(img.reshape(-1, 10, 10))
+#       for i in range(len(img)):
+#         img[i] = img[i].reshape(-1)
+#       images.append(np.array(img))
+#   return images
 
+def load_images(folder):
+  images = []
+  for filename in os.listdir(folder):
+    img = mpimg.imread(os.path.join(folder, filename))
+    if img is not None:
+      img = list(img.reshape(-1, 10, 10))
+      for i in range(len(img)):
+        img[i] = img[i].reshape(-1)
+      images.append(np.array(img))
+  return images
 
-# readjust the dataset to get for each image the 4 indeces (one for each test person)
+images = np.concatenate(load_images("dataset_images"))
+pass
 
-# since we remove some outliers (which were not outliers for all test people)
-# we are going to get that some images have less indeces
-# so we should be careful and understand what we should do about that
+# we know we only want components for 95% of variance, so we rewrite the last two lines
+pca = PCA() # if we know the number of components we write that, otherwise
+                          #   we write the variance we desire (e.g. 0.95)
+scores = pca.fit_transform(images)
+reconstructed = pca.inverse_transform(scores)
+principal_components = pca.components_
 
-# the smiling not-smiling is removing the id of the image --> not good!!
+components_to_analyse = 7
+learnt_components = list(principal_components[:components_to_analyse, :])
+pass
+for i in range(components_to_analyse):
+    learnt_components[i] = learnt_components[i].reshape(10,10)
+
+figure = plt.gcf()
+figure.set_size_inches(0.04444, 0.04444)
+
+for i in range(components_to_analyse):
+    plt.savefig("component_"+str(i)+".png", bbox_inches='tight', pad_inches=0, dpi=226)
+
+# img_compressed = list(pca.inverse_transform(scores))
+#
+# for i in range(len(img_compressed)):
+#   img_compressed[i] = img_compressed[i].reshape(10, 10)
+#
+# img_compressed = np.array(img_compressed).reshape(1140, -1)
+#
+# figure = plt.gcf()
+# figure.set_size_inches(3.54, 5.04) # per mona lisa ok
+# # plt.imshow(img_compressed)
+# # plt.savefig('new_img_6.png', bbox_inches='tight', pad_inches=0, dpi=226)
